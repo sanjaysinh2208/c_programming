c99d493852b4c0660ff928cd150a63db1855399c
sanjaysinh2208 <sanjay.vaghela@teksun.com>
2022-09-15 14:26:36 +0530

    vivek

---
+++ git1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+
+int a,b,c;
+
+printf("enter the number of a = %d ");
+scanf("%d"&a);
+
+printf("enter the number of b = %d ");
+scanf("%d"&b);
+
+c=a+b;
+
+printf("add to number a and b = %d ",c);
+
+}
