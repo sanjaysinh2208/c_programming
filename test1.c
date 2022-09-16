878698d49f5746e5b1ef032c62ed36cddadc2656
sanjaysinh2208 <113412966+sanjaysinh2208@users.noreply.github.com>
2022-09-16 11:24:28 +0530

    Create test1.c

---
+++ test1.c
@@ -0,0 +1,25 @@
+#incclude<stdio.h>
+void main()
+{
+
+printf("hello world ");
+int a,b,c,d,e;
+
+printf("enter thr number of a : ");
+scanf("%d",&a);
+
+printf("enter thr number of b : ");
+scanf("%d",&b);
+
+c=a+b;
+
+printf("sum of a and b = %d ",c);
+
+d=a*b;
+
+printf("multiplication of a and b = %d ",d);
+
+e=a/b;
+
+printf("divsion of a and b  = %d ",e);
+}
